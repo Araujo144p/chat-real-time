f09ab3f349fddca50bfe1c47dcab08961f8a62b9
araujo144p <kircaua5661@gmail.com>
2024-04-25 20:24:23 -0300

    add fastify

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,1 +1,12 @@
-console.log('base rodando!')
+import fastify from 'fastify'
+
+const app = fastify()
+
+
+app.get('/', (request, reply) => {
+    return 'chat real time'
+})
+
+app.listen({ port: 3030 }).then(() => {
+    console.log('server is running...')
+})
